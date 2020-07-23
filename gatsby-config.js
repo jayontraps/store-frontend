32b4345ac34fce65a863cbcb6303ef953396cc44
Jason Righelato <jayontraps@gmail.com>
2020-07-23 07:44:00 +0100

    updated gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: process.env.API_URL || "http://localhost:1337",
         queryLimit: 1000, // Default to 100
         contentTypes: ["product", "range"],
         //If using single types place them in this array.
